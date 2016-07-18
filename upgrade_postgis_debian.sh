33e56cf315a28dd2fef5059f7e650f46ad25bf69
pivaldi <contact@piprime.fr>
2016-07-18 22:56:26 +0200

    minor fix

--- upgrade_postgis_debian.sh
+++ upgrade_postgis_debian.sh
@@ -115,7 +115,7 @@ $ERROR && {
 }
 
 CONFIRM=false
-[ $PG_TARGET_VERSION -eq 0 ] && {
+[ "$PG_TARGET_VERSION" = "0" ] && {
     INFO "Detecting target Postgresql version..."
     PG_TARGET_VERSION=$(pg_lsclusters -h | awk '{print $1}' | sort -n -t. -k 1,2n | tail -1)
     CONFIRM=true
